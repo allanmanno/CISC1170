efa8840e318047aa377f472cff1b2d1639a9a38a
allanmanno <85532363+allanmanno@users.noreply.github.com>
2022-06-14 23:05:43 -0400

    Update CircleStat.java

--- HW1/src/CircleStat.java
+++ HW1/src/CircleStat.java
@@ -1,19 +1,20 @@
+// importing the scanner so that the user can input the radius
 import java.util.Scanner;
 
 class CircleStat{
 	public static void main(String[] args){
-		double Radius, Area, Circumference;
+		double Radius, Area, Circumference; // declaring the variables
 		
 		
-		Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in); // create scanner  
 		
 		System.out.println("What is the radius of the circle?");
-		Radius = in.nextDouble();
+		Radius = in.nextDouble(); // save the user's input
 		
-		
-		
-		Area = Math.PI * Math.pow(Radius, 2);
-		Circumference = Math.PI * (2 * Radius);
+		Area = Math.PI * Math.pow(Radius, 2);   // equation for the area of a circle
+						        // area =  pi * radius^2 
+		Circumference = Math.PI * (2 * Radius); // equation for the circumference of a circle
+							// circ =  pi * 2 * radius
 		
 		System.out.printf("When the radius of a circle is " + Radius + ", the area is " + Area + " and the circumference is " + Circumference);
 		
