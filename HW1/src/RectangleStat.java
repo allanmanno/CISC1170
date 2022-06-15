45638c281041009a109f3cf6d826b78dbc2606a4
allanmanno <85532363+allanmanno@users.noreply.github.com>
2022-06-14 23:12:17 -0400

    Update RectangleStat.java

--- HW1/src/RectangleStat.java
+++ HW1/src/RectangleStat.java
@@ -1,19 +1,21 @@
-import java.util.Scanner;
+import java.util.Scanner;	// import scanner for user's input
 
 class RectangleStat{
 	public static void main(String[] args){
-		double Length, Width, Area, Perimeter;
+		double Length, Width, Area, Perimeter;	// declaring the variables
 		
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("What is the length of the rectangle?");
-		Length = in.nextDouble();
+		System.out.println("What is the length of the rectangle?");	// ask the user for the input
+		Length = in.nextDouble();	// save the input
 		
 		System.out.println("What is the width of the rectangle?");
 		Width = in.nextDouble();
 		
-		Area = Length * Width;
-		Perimeter = (Length + Width) * 2;
+		Area = Length * Width;	// equation for area of rectangle
+					// L*W
+		Perimeter = (Length + Width) * 2;	// equation for perimeter of rectangle
+							// p = 2(l+w)
 		
 		System.out.printf("When one side is " + Length + " and the other side is " + Width + ", the area is " + Area + " and the perimeter is " + Perimeter);
 		
